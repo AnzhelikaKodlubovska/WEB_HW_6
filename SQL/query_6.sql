c404ae5fd16b30f4a9dc633e889e8646b75f93f7
AnzhelikaKodlubovska <angelikagymnasticschannel@gmail.com>
2024-03-09 21:12:40 +0200

    Homework_6_correction

---
+++ SQL/query_6.sql
@@ -0,0 +1,8 @@
+    SELECT gr.name as [group] , s.fullname as student, REVERSE(SUBSTR(REVERSE(s.fullname), 0, CHARINDEX(' ', REVERSE(s.fullname)))) AS last_name
+    FROM students s
+    LEFT JOIN groups gr ON s.group_id = gr.id 
+    WHERE group_id = 1
+    ORDER BY last_name
+
+
+
